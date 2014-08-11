5faae8eaf2c442d43ad0b3ae9af83cc3bb3f10bd
dlancer <dmdpost@gmail.com>
2014-08-11 16:38:03 +0600

    page view and pages tags added

---
+++ pages/templatetags/pages_tags.py
@@ -0,0 +1,115 @@
+from django import template
+from django.template import Context, Template
+from django.utils.html import escape
+
+from pages.conf import settings
+
+register = template.Library()
+
+
+@register.assignment_tag()
+def get_page_object(objects, name):
+    """
+    **Arguments**
+
+    ``objects``
+        all objects
+
+    **Keyword arguments**
+
+    ``name``
+        name for object selection
+
+    :return selected object
+    """
+    selected_object = None
+
+    for obj in objects:
+        if obj.name == name:
+            selected_object = obj
+            break
+    return selected_object
+
+
+@register.assignment_tag(takes_context=True)
+def get_page_object_by_id(context, object_type, oid):
+    """
+    **Arguments**
+
+    ``object_type``
+        object type
+
+    **Keyword arguments**
+
+    ``oid``
+        id for object selection
+
+    :return selected object
+    """
+    if type(oid) != int:
+        raise template.TemplateSyntaxError('page_object_by_id tag requires a integer argument')
+
+    selected_object = None
+
+    try:
+        for obj in context['page']['content'][object_type]:
+            name = '{0:>s}:{1:>s}:{2:>s}:{3:>d}'.format(
+                obj.language, obj.type, context['page']['page'].name, oid
+            )
+            if obj.name == name:
+                selected_object = obj
+                break
+        if selected_object is None:
+            for obj in context['page']['ext_content'][object_type]:
+                name = '{0:>s}:{1:>s}:{2:>s}:{3:>d}'.format(
+                    obj.language, obj.type, context['page']['page'].name, oid
+                )
+                if obj.name == name:
+                    selected_object = obj
+                    break
+    except KeyError:
+        raise template.TemplateSyntaxError('wrong content type: {0:>s}'.format(object_type))
+    return selected_object
+
+
+@register.simple_tag(takes_context=True)
+def page_text_by_id(context, oid):
+    obj = get_page_object_by_id(context, 'text', oid)
+    if obj is None:
+        return None
+    if obj.is_template and settings.PAGES_ALLOW_DJANGO_TEMPLATES:
+        tpl = Template(obj.text)
+        text = tpl.render(Context(context))
+    else:
+        text = escape(obj.text)
+    return text
+
+
+@register.assignment_tag(takes_context=True)
+def get_page_text_by_id(context, oid):
+    return page_text_by_id(context, oid)
+
+
+@register.assignment_tag(takes_context=True)
+def get_raw_page_text_by_id(context, oid):
+    obj = get_page_object_by_id(context, 'text', oid)
+    return obj.text if obj else None
+
+@register.simple_tag(takes_context=True)
+def page_markdown_by_id(context, oid):
+    obj = get_page_object_by_id(context, 'markdown', oid)
+    if obj is None:
+        return None
+    return obj.text
+
+@register.assignment_tag(takes_context=True)
+def get_page_markdown_by_id(context, oid):
+    return page_markdown_by_id(context, oid)
+
+
+@register.assignment_tag(takes_context=True)
+def get_page_image_by_id(context, oid):
+    obj = get_page_object_by_id(context, 'image', oid)
+    return obj.image if obj else None
+
+
---
+++ pages/views/page.py
@@ -0,0 +1,184 @@
+"""Implements page details view for content management system"""
+
+from datetime import datetime
+from hashlib import md5
+
+from django.core.cache import cache
+from django.http import HttpResponseForbidden, Http404
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_control
+from django.views.decorators.http import condition
+from django.shortcuts import render
+from django.utils.translation import get_language
+from django.utils import timezone
+from django.conf import settings as django_settings
+from django.core.exceptions import PermissionDenied
+
+from guardian.shortcuts import get_perms
+
+from pages.conf import settings
+from pages.cache import cache
+from pages.models import Page
+from pages.models import PageContentType
+from pages.models import PageSlugContent
+from pages.models import PageRedirectContent
+from pages.mixins import DecoratorChainingMixin
+
+
+def get_timestamp(slug, language):
+    # try get page modification timestamp for http caching
+    page_cache_version_key = settings.PAGES_PAGE_VERSION_KEY + language + ':' + slug
+    timestamp_cache_key = settings.PAGES_PAGE_CACHE_KEY + language + ':' + slug + '_timestamp'
+    cache_version = cache.get(page_cache_version_key)
+    cache_version = 0 if cache_version is None else cache_version
+    timestamp = cache.get(timestamp_cache_key, version=cache_version)
+    if timestamp is None:
+        timestamp = timezone.now().strftime('%Y-%m-%d %H:%M:%S')
+        cache.set(timestamp_cache_key, timestamp, settings.PAGES_PAGE_CACHE_TIMEOUT, version=cache_version)
+    return timestamp
+
+
+def get_etag(request, **kwargs):
+    is_authenticated = str(request.user.is_authenticated())
+    slug = kwargs.get('slug', None)
+    language = get_language()
+    modified = get_timestamp(slug, language)
+    return md5(modified + language + is_authenticated).hexdigest()
+
+
+def get_last_modified(request, **kwargs):
+    slug = kwargs.get('slug', None)
+    language = get_language()
+    modified = get_timestamp(slug, language)
+    return datetime.strptime(modified, '%Y-%m-%d %H:%M:%S')
+
+
+class PageDetailsView(DecoratorChainingMixin, TemplateView):
+
+    decorators = [cache_control(must_revalidate=True, proxy_revalidate=True, max_age=settings.PAGES_PAGE_HTTP_MAX_AGE),
+                  condition(etag_func=get_etag, last_modified_func=get_last_modified)]
+
+    def get(self, request, **kwargs):
+        is_authenticated = str(request.user.is_authenticated())
+        slug = kwargs.get(settings.PAGES_PAGE_SLUG_NAME, None)
+        if slug is None:
+            raise Http404
+        language = get_language()
+
+        page_cache_key = settings.PAGES_PAGE_CACHE_KEY + language + ':' + slug + ':' + is_authenticated
+        page_cache_version_key = settings.PAGES_PAGE_VERSION_KEY + language + ':' + slug
+
+        # try get cached pages
+        cache_version = cache.get(page_cache_version_key)
+        cache_version = 0 if cache_version is None else cache_version
+        page = cache.get(page_cache_key, version=cache_version)
+
+        if page:
+            # check if login required
+            if page.is_login_required and not request.user.is_authenticated():
+                return HttpResponseRedirect(django_settings.LOGIN_URL)
+
+            # check user view permission
+            if page.is_permission_required:
+                if not 'view_page' in get_perms(request.user, page):
+                    if settings.PAGES_RAISE_403:
+                        raise PermissionDenied
+                    if settings.PAGES_RENDER_403:
+                        return render(request, django_settings.PAGES_TEMPLATE_403, {})
+                    else:
+                        return HttpResponseForbidden()
+
+            # try get pages content from cache
+            page_redirect = cache.get(page_cache_key + 'redirect', version=cache_version)
+            if page_redirect:
+                if page_redirect['permanent']:
+                    return HttpResponsePermanentRedirect(page_redirect['url'])
+                else:
+                    return HttpResponseRedirect(page_redirect['url'])
+
+            page_content = cache.get(page_cache_key + 'content', version=cache_version)
+            page_ext_content = cache.get(page_cache_key + 'ext_content', version=cache_version)
+
+        else:
+            # try request pages from db and store pages content in cache
+            slugs = PageSlugContent.objects.filter(slug=slug, language=language)
+            if not slugs:
+                raise Http404
+            slug = slugs[0]
+            try:
+                page = Page.objects.published().filter(pk=slug.page_id)[0]
+
+                # cache pages content
+                cache.set(page_cache_version_key, cache_version)
+                cache.set(page_cache_key, page, version=cache_version)
+
+                # check if login required
+                if page.is_login_required and not request.user.is_authenticated():
+                    return HttpResponseRedirect(django_settings.LOGIN_URL)
+
+                # check user view permission
+                if page.is_permission_required:
+                    if not 'view_page' in get_perms(request.user, page):
+                        if settings.PAGES_RAISE_403:
+                            raise PermissionDenied
+                        if settings.PAGES_RENDER_403:
+                            return render(request, django_settings.PAGES_TEMPLATE_403, {})
+                        else:
+                            return HttpResponseForbidden()
+
+                page_content = {}
+                page_ext_content = {}
+                page_redirect = {}
+
+                # check if this pages has redirect
+                try:
+                    redirect = PageRedirectContent.objects.get(page=page, language=language)
+                    page_redirect.update({
+                        'url': redirect.get_redirect_url(request),
+                        'permanent': redirect.is_permanent
+                    })
+                    cache.set(page_cache_key + 'redirect', page_redirect, version=cache_version)
+                    if page_redirect['permanent']:
+                        return HttpResponsePermanentRedirect(page_redirect['url'])
+                    else:
+                        return HttpResponseRedirect(page_redirect['url'])
+
+                except PageRedirectContent.DoesNotExist:
+                    content_types = PageContentType.objects.filter(is_extended=False)
+                    for content_type in content_types:
+                        content_class = Page.get_content_class(content_type.type)
+                        content = content_class.objects.filter(page=page, language=language)
+                        if content:
+                            page_content.update({content_type.type: content})
+
+                    if settings.PAGES_PAGE_EXT_CONTENT_TYPES:
+                        ext_content_types = PageContentType.objects.extended()
+                        for content_type in ext_content_types:
+                            content_class = Page.get_content_class(content_type.type)
+                            ext_content = content_class.objects.filter(page=page, language=language)
+                            if ext_content:
+                                page_ext_content.update({content_type.type: ext_content})
+
+                if page_content:
+                    cache.set(page_cache_key + 'content', page_content, version=cache_version)
+                if page_ext_content:
+                    cache.set(page_cache_key + 'ext_content', page_ext_content, version=cache_version)
+
+            except IndexError:
+                raise Http404
+
+        context = {
+            'page': {
+                'page': page,
+                'slug': slug,
+                'content': page_content,
+                'ext_content': page_ext_content,
+                'cache_key': page_cache_key,
+                'cache_version': cache_version,
+                'timeout': settings.PAGES_PAGE_CACHE_TIMEOUT,
+            }
+
+        }
+
+        return render(request, page.get_template(), context)
