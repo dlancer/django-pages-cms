74678d71541f52d399831f81f01bd318d80f4c23
dlancer <dmdpost@gmail.com>
2014-08-17 04:44:49 +0600

    FormMessageMixin added

--- pages/mixins.py
+++ pages/mixins.py
@@ -1,3 +1,7 @@
+"""Implements decorator chaining mixin for class based view"""
+
+from django.contrib import messages
+
 
 class DecoratorChainingMixin(object):
     def dispatch(self, *args, **kwargs):
@@ -7,3 +11,29 @@ class DecoratorChainingMixin(object):
         for decorator in decorators:
             base = decorator(base)
         return base(*args, **kwargs)
+
+
+class FormMessageMixin(object):
+    """Make it easy to display notification messages when using Class Based Views"""
+
+    def delete(self, request, *args, **kwargs):
+        message = None
+        if hasattr(self, 'form_delete_message'):
+            message = self.form_delete_message
+        elif hasattr(self, 'form_valid_message'):
+            message = self.form_valid_message
+        if message is not None:
+            messages.success(self.request, message)
+        return super(FormMessageMixin, self).delete(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        message = self.form_valid_message if hasattr(self, 'form_valid_message') else None
+        if message is not None:
+            messages.success(self.request, message)
+        return super(FormMessageMixin, self).form_valid(form)
+
+    def form_invalid(self, form):
+        message = self.form_invalid_message if hasattr(self, 'form_invalid_message') else None
+        if message is not None:
+            messages.success(self.request, message)
+        return super(FormMessageMixin, self).form_invalid(form)
