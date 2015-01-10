1df0a5ba33816b4ce15d14d5378693e20119f8c7
dlancer <dmdpost@gmail.com>
2015-01-11 01:06:47 +0500

    version bump to 0.0.13

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,4 +1,4 @@
-VERSION = (0, 0, 12)
+VERSION = (0, 0, 13)
 
 
 def get_version():
@@ -7,7 +7,7 @@ def get_version():
 
 __author__ = 'dlancer'
 __docformat__ = 'restructuredtext en'
-__copyright__ = 'Copyright 2014, dlancer'
+__copyright__ = 'Copyright 2014-2015, dlancer'
 __license__ = 'BSD'
 __version__ = get_version()
 __maintainer__ = 'dlancer'
