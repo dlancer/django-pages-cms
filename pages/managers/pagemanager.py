448060fd40cc9f1b2c7d6389d7629b00f8792584
dlancer <dmdpost@gmail.com>
2015-08-10 18:03:06 +0500

    PageManager crash fixed on page delete operation

--- pages/managers/pagemanager.py
+++ pages/managers/pagemanager.py
@@ -1,30 +1,26 @@
 import django
 
-from django.db import models
+from mptt.managers import TreeManager, TreeQuerySet
 
 from pages.conf import settings
 
 
-class PageManager(models.Manager):
-    def get_queryset(self):
-        super_ = super(PageManager, self)
-        # fix get_query_set for old Django releases
-        if django.VERSION < (1, 6):
-            qs = super_.get_query_set()
+class PageManager(TreeManager):
+
+    def get_queryset(self, *args, **kwargs):
+        """
+        Ensures that this manager always returns nodes in tree order.
+        """
+        if django.VERSION < (1, 7):
+            qs = TreeQuerySet(self.model, using=self._db)
         else:
-            qs = super_.get_queryset()
+            qs = super(TreeManager, self).get_queryset(*args, **kwargs)
 
         # Restrict operations to pages on the current site if needed
         if settings.PAGES_HIDE_SITES and settings.PAGES_USE_SITE_ID:
-            return qs.filter(sites=settings.SITE_ID)
+            return qs.order_by(self.tree_id_attr, self.left_attr).filter(sites=settings.SITE_ID)
         else:
-            return qs
-
-    get_query_set = get_queryset
-
-    def get_queryset_compat(self):
-        get_queryset = (self.get_query_set if hasattr(self, 'get_query_set') else self.get_queryset)
-        return get_queryset()
+            return qs.order_by(self.tree_id_attr, self.left_attr)
 
     def on_site(self, site_id=None):
         """Return a :class:`QuerySet` of pages that are published on the site
@@ -35,9 +31,9 @@ class PageManager(models.Manager):
         if settings.PAGES_USE_SITE_ID:
             if not site_id:
                 site_id = settings.SITE_ID
-            return self.get_queryset_compat().filter(sites=site_id)
+            return self.get_queryset().filter(sites=site_id)
         return self.all()
 
     def published(self):
         """Return queryset with only published pages"""
-        return self.get_queryset_compat().filter(is_published=True, is_hidden=False)
+        return self.get_queryset().filter(is_published=True, is_hidden=False)
