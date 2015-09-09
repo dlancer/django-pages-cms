f745d4bcd9eea30225f22450cadb870df6d0fd80
dlancer <dmdpost@gmail.com>
2015-09-09 12:53:41 +0500

    pep8 code clean

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import patterns, url
 from pages.views import PageDetailsView
 
 urlpatterns = patterns('pages.views',
-    # pages
-    url(r'^(?P<slug>[-\w]+)/$', PageDetailsView.as_view(), name='page_show'),
-)
+                       # pages
+                       url(r'^(?P<slug>[-\w]+)/$', PageDetailsView.as_view(), name='page_show'),
+                       )
