4e0d28bc296a63fec715237cc7e9ae6fc3677d7d
dlancer <dmdpost@gmail.com>
2015-10-21 16:56:08 +0500

    page type field added to page model

---
+++ pages/migrations/0002_add_field_ptype.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pages', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='page',
+            name='ptype',
+            field=models.CharField(default='page', max_length=64),
+        ),
+    ]
