fed53ba93381a3d1fc8d49a47582851d679f1793
dlancer <dmdpost@gmail.com>
2015-12-02 14:30:53 +0500

    outdated legacy code removed

--- pages/cache.py
+++ pages/cache.py
@@ -1,10 +1,5 @@
-import django
+from django.core.cache import caches
 
 from pages.conf import settings
 
-if django.VERSION[:2] >= (1, 7):
-    from django.core.cache import caches
-    cache = caches[settings.PAGES_CACHE_BACKEND]
-else:
-    from django.core import cache as cache_module
-    cache = cache_module.get_cache(settings.PAGES_CACHE_BACKEND)
+cache = caches[settings.PAGES_CACHE_BACKEND]
