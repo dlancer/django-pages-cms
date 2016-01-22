f2ebe62e366826d4e3df56e964692ed365f875f9
dlancer <dmdpost@gmail.com>
2016-01-23 02:18:29 +0500

    version bump to 0.2.1

--- pages/__init__.py
+++ pages/__init__.py
@@ -1,15 +1,21 @@
-VERSION = (0, 2, 0)
+VERSION = (0, 2, 1, 'stable')
+
+
+def get_release():
+    return '-'.join([get_version(), VERSION[-1]])
 
 
 def get_version():
-    """Returns the version as a human-format string."""
-    return '.'.join([str(i) for i in VERSION])
+    """
+    Returns only digit parts of version.
+    """
+    return '.'.join(str(i) for i in VERSION[:3])
 
 __author__ = 'dlancer'
 __docformat__ = 'restructuredtext en'
 __copyright__ = 'Copyright 2014-2016, dlancer'
 __license__ = 'BSD'
-__version__ = get_version()
+__version__ = get_release()
 __maintainer__ = 'dlancer'
 __email__ = 'dmdpost@gmail.com'
 __status__ = 'Development'
